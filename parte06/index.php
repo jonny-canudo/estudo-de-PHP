fc65ff77ca3a5474211ba8a5d06c92e11e90127d
jonny-jonny <jpcanuto789@gmail.com>
2024-07-29 10:07:13 -0300

    segundo commit

---
+++ parte06/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .error{
+            color:red;
+
+        }
+    </style>
+</head>
+<body>
+    <form method="POST" action="">
+        <h1>formulario php</h1>
+            nome: <input  name="nome"  type="text"><br><br>
+            E-mail: <input  name="email"  type="text"><br><br>
+            website: <input  name="email"  type="text"><br><br>
+            Comentário: <textarea name="comentario" ></textarea><br><br>
+            
+            gênero: <input name="genero" type="radio"> Masculino
+            <input name="genero" type="radio">Feminino
+            <input name="genero" type="radio">Outro
+
+
+
+    </form>
+    
+</body>
+</html>
