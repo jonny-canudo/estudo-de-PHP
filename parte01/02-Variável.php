4975ae2bb5a798076dc8e7b83dac0c80a76abeff
jonny-jonny <jpcanuto789@gmail.com>
2024-07-29 10:12:07 -0300

    segundo commit

--- parte01/02-Variável.php
+++ parte01/02-Variável.php
@@ -3,11 +3,11 @@
 $Variável1 = "Valor da primeira Variável.";
 $Variável2 = "Valor da segunda Variável.";
 $Variável3 = "Valor da terceira Variável.";
-
+$Variável4 = "Valor da quarta Variável.";
 echo "\n$Variável1\n";
 echo "\n$Variável2\n";
 echo "\n$Variável3\n";
-
+echo " \n\n$Variável4\n\n";
 
 ?>
 
